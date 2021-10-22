8e28d27739dcbbb74bd8c0165ecbde0ac56f0b36
KeyCrusader <63306846+KeyCrusader@users.noreply.github.com>
2021-10-22 09:27:46 +0100

    Add files via upload

--- HideFromRadioList.js
+++ HideFromRadioList.js
@@ -3,20 +3,24 @@ if (('{#product}' !== 'Service' || '{#is_authenticated}' === 'false')) {
 	// Loop through all radio and checkbox elements
 	Array.from(document.querySelectorAll('.radio,.checkbox')).forEach(selectElement => {
 		// Loop through each span object within radio (these hold the input and label)
-		Array.from(selectElement.children).forEach(spanElement => {
-			let hiddenFlag = false;
-			
-			// Loop through input and label and find inputs with _ at the start of the value
-			Array.from(spanElement.children).forEach(listElement => {
-				if (listElement.nodeName === 'INPUT' && listElement.value[0] === '_') {
-					hiddenFlag = true;
-				}				
+		if (selectElement.hasChildNodes()) {
+			Array.from(selectElement.children).forEach(spanElement => {
+				let hiddenFlag = false;
+				
+				// Loop through input and label and find inputs with _ at the start of the value
+				if (spanElement.hasChildNodes()) {
+					Array.from(spanElement.children).forEach(listElement => {
+						if (listElement.nodeName === 'INPUT' && listElement.value[0] === '_') {
+							hiddenFlag = true;
+						}				
+					});
+				}
+				
+				// If the input element was private then hide the span element
+				if (hiddenFlag) {
+					spanElement.classList.add('hidden');
+				}
 			});
-			
-			// If the input element was private then hide the span element
-			if (hiddenFlag) {
-				spanElement.classList.add('hidden');
-			}
-		});
+		}
 	});
 }
