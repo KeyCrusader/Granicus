dfa616d4eadad2eab95ce2106ae2f918729d1f3b
KeyCrusader <63306846+KeyCrusader@users.noreply.github.com>
2021-10-22 09:44:34 +0100

    Add files via upload

---
+++ HideFromRadioList.js
@@ -0,0 +1,26 @@
+// Checks if the user is logged in through service, if not hides the listed fields
+if (('{#product}' !== 'Service' || '{#is_authenticated}' === 'false')) {
+	// Loop through all radio and checkbox elements
+	Array.from(document.querySelectorAll('.radio,.checkbox')).forEach(selectElement => {
+		// Loop through each span object within radio (these hold the input and label)
+		if (selectElement.hasChildNodes()) {
+			Array.from(selectElement.children).forEach(spanElement => {
+				let hiddenFlag = false;
+				
+				// Loop through input and label and find inputs with _ at the start of the value
+				if (spanElement.hasChildNodes()) {
+					Array.from(spanElement.children).forEach(listElement => {
+						if (listElement.nodeName === 'INPUT' && listElement.value[0] === '_') {
+							hiddenFlag = true;
+						}				
+					});
+				}
+				
+				// If the input element was private then hide the span element
+				if (hiddenFlag) {
+					spanElement.classList.add('hidden');
+				}
+			});
+		}
+	});
+}
